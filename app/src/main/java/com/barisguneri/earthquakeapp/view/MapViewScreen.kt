0d416fc842b0377bac0fa4fc06c203de7c3ebbbd
Barış Güneri <barisgnr.tr@gmail.com>
2023-11-19 22:01:28 +0300

    marker add

--- app/src/main/java/com/barisguneri/earthquakeapp/view/MapViewScreen.kt
+++ app/src/main/java/com/barisguneri/earthquakeapp/view/MapViewScreen.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.barisguneri.earthquakeapp.R
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.infowindow.InfoWindow
 
 @Composable
 fun OsmdroidMapView() {
@@ -15,14 +19,25 @@ fun OsmdroidMapView() {
         factory = { context ->
             MapView(context).apply {
                 setTileSource(TileSourceFactory.USGS_TOPO)
+                this.setTileSource(TileSourceFactory.MAPNIK)
+                this.setBuiltInZoomControls(true)
+                this.setMultiTouchControls(true)
                 setOnClickListener {
                 }
             }
         },
         update = { view ->
-            // Set zoom level and center coordinates
             view.controller.setZoom(9.0)
             view.controller.setCenter(GeoPoint(41.0719461, 28.200803))
+            val marker = Marker(
+                view
+            )
+            marker.position = GeoPoint(41.0719461, 28.200803)
+            marker.title = "Test 6tgsf"
+            marker.subDescription = "fgjmksdöfl ıjvmkfvmık trvmmırmvrtvfv ver er erferfefr"
+
+            val overlayManager = view.getOverlayManager()
+            overlayManager.add(marker)
         }
     )
 }
